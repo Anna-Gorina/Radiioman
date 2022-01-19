139711b90813be638f9b3af937ac4dc9bb5e3bce
Anna-Gorina <gorinaanna1992@gmail.com>
2022-01-19 18:14:26 +0400

    Init commit

---
+++ src/main/java/ru/netology/domine/Radio.java
@@ -0,0 +1,57 @@
+package ru.netology.domine;
+
+public class Radio {
+
+    private int currentRadioStation;
+    private int currentVolume;
+
+    public void next() {
+        currentRadioStation++;
+        if (currentRadioStation > 9) {
+            currentRadioStation = 0;
+        }
+    }
+
+    public void prev() {
+        currentRadioStation--;
+        if (currentRadioStation < 0) {
+            currentRadioStation = 9;
+        }
+    }
+
+    public void increaseValue() {
+        if (currentVolume < 10) {
+            currentVolume = currentVolume + 1;
+        }
+
+    }
+
+    public void decreaseValue() {
+        if (currentVolume > 0) {
+            currentVolume = currentVolume - 1;
+        }
+
+    }
+
+    public int getCurrentRadioStation() {
+        return currentRadioStation;
+    }
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation > 9 && currentRadioStation < 0) {
+            return;
+        }
+        this.currentRadioStation = currentRadioStation;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    public void setCurrentVolume(int currentVolume) {
+        if (currentVolume > 10 && currentVolume < 0) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+}
---
+++ src/test/java/ru/netology/domine/RadioTest.java
@@ -0,0 +1,87 @@
+package ru.netology.domine;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+
+    @Test
+public void shouldBeLastNumberOfCurrentRadioStation() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(9);
+        radio.next();
+        assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void shouldBeNextNumberOfCurrentRadioStation() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(5);
+        radio.next();
+        assertEquals(6, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void shouldBeNullOnCurrentRadioStation() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(0);
+        radio.prev();
+        assertEquals(9, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void shouldBePrevCurrentRadioStation() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(5);
+        radio.prev();
+        assertEquals(4, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void shouldBeIncreaseCurrentVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(5);
+        radio.increaseValue();
+        assertEquals(6, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldBeDecreaseCurrentVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(3);
+        radio.decreaseValue();
+        assertEquals(2, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldBeMaxValueOfVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(10);
+        radio.increaseValue();
+        assertEquals(10, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldBeMinValueOfVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+        radio.decreaseValue();
+        assertEquals(0, radio.getCurrentVolume());
+    }
+    @Test
+    public void shouldBeIncreaseMoreMaxValueOfVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(15);
+        radio.increaseValue();
+        assertEquals(15, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void shouldBeDecreaseOverMinValueOfVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(-2);
+        radio.decreaseValue();
+        assertEquals(-2, radio.getCurrentVolume());
+    }
+}
